fa882fd124e59e4fb68b7cba185b2d54b95bdaa0
Rahul <rahulna@thoughtworks.com>
2016-11-23 20:26:13 +0530

    Fixed some failing tests in RootNode

--- grammarTest.js
+++ grammarTest.js
@@ -3,9 +3,11 @@ var jison = require("jison");
 var fs = require("fs");
 
 describe('grammar', function(){
-
-	var bnf = fs.readFileSync("./grammar.jison", "utf8");
-	var parser = new jison.Parser(bnf);
+	var parser;
+	beforeEach(function(){
+		var bnf = fs.readFileSync("./grammar.jison", "utf8");
+		parser = new jison.Parser(bnf);
+	});
 
 	it('@evaluate x=10;5+x*2;', function(){
 		var  input = 'x=10;5+x*2;'
@@ -22,6 +24,13 @@ describe('grammar', function(){
 	it('@evaluate x=12;y=10;z=x*12+y/5;z;', function(){
 		var  input = 'x=12;y=10;z=x*12+y/5;z;'
 		var expected = 146;
+		assert.equal(expected, parser.parse(input).evaluate());
+	});
+
+	it('should throw exception @evaluate x+2;x=10;', function(){
+		var  input = 'x+2;x=10;'
+		console.log(parser.parse(input));
+		var expected = 12;
 		assert.equal(expected, parser.parse(input).evaluate());
 	});
 });
